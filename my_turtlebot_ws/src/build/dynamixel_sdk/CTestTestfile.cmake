35d7f209f11071cba28909aaed3bf94ae46f5350
MINWOO <woolee8572@naver.com>
2025-06-15 20:45:09 +0900

    Final cleaned version with zip file included

--- my_turtlebot_ws/src/build/dynamixel_sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/my_turtlebot_ws/src/DynamixelSDK/ros/dynamixel_sdk
-# Build directory: /home/pi/my_turtlebot_ws/src/build/dynamixel_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- my_turtlebot_ws/src/build/ld08_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/my_turtlebot_ws/src/ld08_driver
-# Build directory: /home/pi/my_turtlebot_ws/src/build/ld08_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
